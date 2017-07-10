3b006577b6ee100dff10582b20e449f755a3116f
KapSSegit config --global user.email kapranovstanislav@gmail.comgit helpgit config --global user.name KapSSe <kapranovstanislav@gmail.com>
2017-07-10 14:06:25 +0300

    fixed footer

--- app/js/preloader.js
+++ app/js/preloader.js
@@ -1,5 +1,5 @@
         $(window).on('load', function() {  
             $('#status').fadeOut();  
-            $('#preloader').delay(350).fadeOut('slow');  
+            $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
             $('body').delay(350).css({'overflow':'visible'});
           })
